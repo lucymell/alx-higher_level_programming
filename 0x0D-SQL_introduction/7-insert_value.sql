ec4dfb89b07b72a2e9ade3fb805e0ef9681e82c8
lucymell <99334158+lucymell@users.noreply.github.com>
2022-08-09 13:35:01 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in a table
--- Query to insert a new row in the table first_table
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+-- Insert a new row in table
+INSERT INTO first_table (id, name) VALUES(89, 'Holberton School');
